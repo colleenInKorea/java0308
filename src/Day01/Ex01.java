9d94ea1297f901006bc94ab836b11c9a76b00dc1
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2022-03-08 11:46:17 +0900

    Initial commit

---
+++ src/Day01/Ex01.java
@@ -0,0 +1,25 @@
+package Day01;
+
+public class Ex01 {
+    public static void main(String[] args) {
+
+        int noodleCup ;
+
+        noodleCup = 850;
+
+        // 변수출력
+        System.out.println(noodleCup);
+        System.out.println("육개장 가격 : " + noodleCup);
+
+        // 줄바꿈
+        System.out.println("안녕하세요. 반갑습니다. ");
+
+        // println 을 통한 줄 바꿈
+        System.out.println("안녕하세요.");
+        System.out.println("반갑습니다.");
+
+        // \n 을 통한
+
+
+    }
+}
