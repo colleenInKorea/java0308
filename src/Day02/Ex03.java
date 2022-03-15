36772ccc3ca2ab715a1cb01514cf5f1a381af239
50305-PC15\User <sjhdka@gmail.com>
2022-03-15 15:42:56 +0900

    Changes_202203151541

--- src/Day02/Ex03.java
+++ src/Day02/Ex03.java
@@ -6,18 +6,18 @@ public class Ex03 {
     public static void main(String[] args) {
 
         Map<String, Object> Information = new HashMap<>();
-        Information.put("이름", "이도경");
-        Information.put("나이", 25);
+        Information.put("이름", "이또또");
+        Information.put("나이", 24);
         Information.put("학교", "동아방송예술대");
         Information.put("성별", "여성");
         Information.put("주 프로그래밍언어", "자바");
 
         Map<String, Object> Information1 = new HashMap<>();
-        Information1.put("이름", "이도희");
-        Information1.put("나이", 24);
+        Information1.put("이름", "이또치");
+        Information1.put("나이", 23);
         Information1.put("학교", "경희대학교");
         Information1.put("성별", "여성");
-        Information1.put("특기", "디자인");
+        Information1.put("특기", "캡스톤디자인");
 
         List<Map<String, Object>> PersonenInformation = new ArrayList<>();
         PersonenInformation.add(Information);
