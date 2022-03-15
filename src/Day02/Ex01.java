0d660795aecb139071e7d7f2d0b98732dc2ee46d
50305-PC15\User <sjhdka@gmail.com>
2022-03-15 15:55:21 +0900

    Changes_202203151555

--- src/Day02/Ex01.java
+++ src/Day02/Ex01.java
@@ -13,7 +13,14 @@ public class Ex01 {
         System.out.println("b 값을 입력하세요");
         numberB = scan.nextInt();
         int sum = numberA + numberB;
-        System.out.print("a+b의 값은 " + sum + " 입니다.");
+        System.out.println("a+b의 값은 " + sum + " 입니다.");
+        sum = numberA - numberB;
+        System.out.println("a - b의 값은 " + sum + "입니다. ");
+        sum = numberA * numberB;
+        System.out.println("a x b 의 값은 " + sum + "입니다.");
+        sum = numberA / numberB;
+        int exchange = numberA % numberB;
+        System.out.println("a % b의  몫은 " + sum + "나머지는 " + exchange + " 입니다.");
 
     }
 }
