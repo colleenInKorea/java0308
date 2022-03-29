395b16c66a19cc503724df8be22395ca1a95de0f
User <ledgy98@gmail.com>
2022-03-29 16:31:14 +0900

    commit 20220329

--- src/Day04/Test06.java
+++ src/Day04/Test06.java
@@ -7,7 +7,7 @@ public class Test06 {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("원하는 수식을 쓰시오 (숫자와 연산자를 띄어서 써야 합니다.)");
-        String [] mathEx = scan.next().split(" ");
+        String [] mathEx = scan.nextLine().split(" ");
         int number1, number2;
         char operator;
 
@@ -16,7 +16,7 @@ public class Test06 {
         operator = mathEx[1].charAt(0);
         number2 = Integer.parseInt(mathEx[2]);
 
-        switch (operator) {
+       switch (operator) {
             case '+' -> System.out.println(number1 + " + " + number2 + " = " + (number1 + number2));
             case '-' -> System.out.println(number1 + " - " + number2 + " = " + (number1 - number2));
             case '*' -> System.out.println(number1 + " * " + number2 + " = " + (number1 * number2));
