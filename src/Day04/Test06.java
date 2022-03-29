267cfb86f8f85714250bb207cd008fd8a1c0a3c2
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2022-03-29 21:11:36 +0900

    Changes 220329

--- src/Day04/Test06.java
+++ src/Day04/Test06.java
@@ -8,13 +8,10 @@ public class Test06 {
 
         System.out.println("원하는 수식을 쓰시오 (숫자와 연산자를 띄어서 써야 합니다.)");
         String [] mathEx = scan.nextLine().split(" ");
-        int number1, number2;
-        char operator;
 
-
-        number1 = Integer.parseInt(mathEx[0]);
-        operator = mathEx[1].charAt(0);
-        number2 = Integer.parseInt(mathEx[2]);
+        int number1 = Integer.parseInt(mathEx[0]);
+        char operator = mathEx[1].charAt(0);
+        int number2 = Integer.parseInt(mathEx[2]);
 
        switch (operator) {
             case '+' -> System.out.println(number1 + " + " + number2 + " = " + (number1 + number2));
