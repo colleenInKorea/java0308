f9493f75c31302d6358e29fafbb685c505bf245a
colleen <ledgy98@gmail.com>
2022-04-05 16:22:09 +0900

    Commit 220405

--- src/Day05/Test01.java
+++ src/Day05/Test01.java
@@ -10,7 +10,7 @@ public class Test01 {
         for(int i = 1; i < 10; i ++){
             System.out.println();
             for (int j = 1; j < 10; j++){
-                System.out.print( j + " x " + i + " = " + (i * j) + "  ");
+                System.out.print(  j  + " x " + i + " = " + (i * j) + "  ");
             }
         }
     }
