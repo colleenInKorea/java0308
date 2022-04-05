3f3d928beb5be06486de05b25bfa88ad5304a471
colleen <ledgy98@gmail.com>
2022-04-05 14:00:30 +0900

    Commit 220405

--- src/Day05/Test01.java
+++ src/Day05/Test01.java
@@ -4,8 +4,9 @@ public class Test01 {
     public static void main(String[] args) {
 
         for(int i = 1; i < 10; i ++){
+            System.out.println();
             for (int j = 1; j < 10; j++){
-                System.out.println(i + " x " + j + " = " + (i * j));
+                System.out.print(i + " x " + j + " = " + (i * j) + "  ");
             }
         }
 
