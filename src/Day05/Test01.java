a471a7a750e4324aec6b4a17394158570621f456
colleen <ledgy98@gmail.com>
2022-04-05 16:16:29 +0900

    Commit 220405

--- src/Day05/Test01.java
+++ src/Day05/Test01.java
@@ -3,10 +3,14 @@ package Day05;
 public class Test01 {
     public static void main(String[] args) {
         //구구단 출력하기
+        for (int i = 1; i <= 9; i++){
+            System.out.printf("# 제%d단 #  ", i);
+        }
+        System.out.println();
         for(int i = 1; i < 10; i ++){
             System.out.println();
             for (int j = 1; j < 10; j++){
-                System.out.print(i + " x " + j + " = " + (i * j) + "  ");
+                System.out.print( j + " x " + i + " = " + (i * j) + "  ");
             }
         }
     }
