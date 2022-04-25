614ef9ab9e0860378014f247e1bd1d5783ae7b75
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2022-04-25 23:53:12 +0900

    Changes 220425

---
+++ src/midterm/test07.java
@@ -0,0 +1,21 @@
+package midterm;
+
+public class test07 {
+    static int diff(char a, char b) {
+        if(a>b) {
+            return a-b;
+        }else {
+            return b-a;
+        }
+    }
+    public static void main(String[] args) {
+// TODO Auto-generated method stub
+        Scanner s =new Scanner(System.in);
+        char a,b;
+        System.out.print("문자1을 입력:");
+        a=s.next().charAt(0);
+        System.out.print("문자2를 입력:");
+        b=s.next().charAt(0);
+        System.out.printf("두 문자의 차이 ==> %d",diff(a,b));
+    }
+}
