8ad10ee3ae58565629f176abdd4195034ad4b363
idogyeong <ledgy98@gmail.com>
2022-05-03 14:49:16 +0900

    commit 220502

---
+++ src/Day09/Test01.java
@@ -0,0 +1,18 @@
+package Day09;
+
+public class Test01 {
+    public static void main(String[] args) {
+        int a = 1234;
+        float b = 100.12345f;
+        char c = 'k';
+        String s = "IT_Cookbook";
+
+        System.out.printf("정수의 10진수 ==> %d", a);
+        System.out.printf("정수의 16진수 ==> %x", a);
+        System.out.printf("정수의 8진수 ==> %o", a);
+        System.out.printf("실수 ==> %o", a);
+        System.out.printf("실수(공학용) ==> %o", a);
+        System.out.printf("문자 ==> %c", a);
+        System.out.printf("문자열 ==> %s", s);
+    }
+}
