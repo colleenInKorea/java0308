50b471e6959d1f566980ad237cdaeda7674b1778
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2022-11-03 23:04:22 +0900

    Changes 220425

---
+++ src/PrivateTest/Day03.java
@@ -0,0 +1,11 @@
+package PrivateTest;
+
+public class Day03 {
+    public static void main(String[] args) {
+        char c = 'S';
+        int i = 10;
+
+        System.out.println(c);
+        System.out.println(i);
+    }
+}
---
+++ src/PrivateTest/Day04.java
@@ -0,0 +1,11 @@
+package PrivateTest;
+
+import java.lang.reflect.Array;
+
+public class Day04 {
+    public static void main(String[] args) {
+        int[] scores = {10, 20, 30};
+        System.out.println(scores[2]);
+
+    }
+}
