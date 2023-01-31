3c81b1b347f96c4730b965df76f80ddfe876f3f0
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-01-31 14:57:50 +0900

    Day18_1

---
+++ src/Day18/Test02.java
@@ -0,0 +1,23 @@
+package Day18;
+
+public class Test02 {
+    public String solution(String s, int n) {
+        String answer = "";
+
+
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if(Character.isLowerCase(ch)) { //소문자
+                ch = (char) ((ch - 'a' + n) % 26 + 'a');
+            } else if(Character.isUpperCase(ch)) { //대문자
+                ch = (char) ((ch - 'A' + n) % 26 + 'A');
+            }
+
+            answer += ch;
+        }
+
+
+        return answer;
+    }
+}
