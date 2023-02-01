fa883f47751aa3b1f79a9a933d48e852c53e360e
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-01 20:32:10 +0900

    Day19

--- src/ExtraTest/Test02_1.java
+++ src/ExtraTest/Test02_1.java
@@ -1,7 +1,7 @@
 package ExtraTest;
 import java.util.Scanner;
 
-public class Main {
+public class Test02_1 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
