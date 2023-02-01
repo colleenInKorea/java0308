4eda63b8d4b8bfa88982203a749d4c1ea1e23746
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-01 16:36:33 +0900

    Day19

---
+++ src/Day19/Test02.java
@@ -0,0 +1,13 @@
+package Day19;
+
+public class Test02 {
+    public int solution(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
