63a0dddddb16573e1e6cfd6de5c3df79ece940a7
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-01 16:57:32 +0900

    Day19

--- src/Day19/Test05.java
+++ src/Day19/Test05.java
@@ -1,4 +1,36 @@
 package Day19;
-
+import java.util.HashSet;
 public class Test05 {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+
+        HashSet<Integer> resSet = new HashSet<>();
+        HashSet<Integer> loSet = new HashSet<>();
+
+        for(int i: reserve) {
+            resSet.add(i);
+        }
+
+        for(int i: lost) {
+            if(resSet.contains(i)) {
+                resSet.remove(i);
+            }
+            else {
+                loSet.add(i);
+            }
+        }
+
+        for(int i: resSet) {
+            if(loSet.contains(i-1)) {
+                loSet.remove(i - 1);
+            }
+            else if(loSet.contains(i+1)) {
+                loSet.remove(i + 1);
+            }
+        }
+
+        answer = n - loSet.size();
+
+        return answer;
+    }
 }
