e34555b191ba2d7b0a9bcd1c793ac1a20587867a
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-02 11:19:32 +0900

    Day20

--- src/MainTest/Test01.java
+++ src/MainTest/Test01.java
@@ -1,4 +1,21 @@
 package MainTest;
 
 public class Test01 {
+    public int solution(int num) {
+        int answer = 0;
+        int[] changes = {500, 100, 50, 10};
+        int change = 1000- num;
+        for (int i =0; i < changes.length; i++){
+            if(change == 0) break;
+            answer += change/changes[i];
+            change %= changes[i];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Test01 method = new Test01();
+        int num1 = 160;
+        System.out.println(method.solution(num1));
+    }
 }
--- src/MainTest/Test02.java
+++ src/MainTest/Test02.java
@@ -1,4 +1,23 @@
 package MainTest;
 
 public class Test02 {
+    public int solution(String s) {
+        int count = 0;
+        int sum = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'O'){
+                count++;
+            }else {
+                count = 0;
+            }
+            sum+= count;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Test02 method = new Test02();
+        String s = "OXOOOXXXOXOOXOOOOOXO";
+        System.out.println(method.solution(s));
+    }
 }
