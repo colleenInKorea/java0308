4a14ed043d2f0e6e8d4c55ce9d5812deb4142c81
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-03 16:24:40 +0900

    Day21

--- src/Day21/Taxi.java
+++ src/Day21/Taxi.java
@@ -3,6 +3,102 @@ package Day21;
 public class Taxi extends PublicTransport{
     private String target;
     private int targetDistance;
+    private int disPerFee;
 
-    Taxi()
+    public int getTotalFee() {
+        return totalFee;
+    }
+
+    private int totalFee;
+
+    Taxi(int number, int amount){
+        this.setNumber(number);
+        this.setAmount(amount);
+       this.setMaxPassenger(4);
+       this.setNowPassenger(0);
+       this.setFee(3000);
+       this.totalFee = 0;
+    }
+
+    @Override
+    public String startDriving(){
+        String answer = "";
+        if(this.getAmount() < 10){
+            this.setState("운행불가");
+            System.out.println(this.getState() + " 주유를 해주세요.");
+        }else {
+            this.setState("일반");
+           answer = "상태 :" + this.getState();
+        }
+        return answer;
+    }
+
+
+    public String insertPassenger(int num, String target, int targetDistance) {
+        String answer = "";
+        if(this.getState().equals("일반")){
+            this.setNowPassenger(this.getNowPassenger() + num);
+            this.setMaxPassenger(4 - this.getNowPassenger());
+            if(this.getNowPassenger() > 4){
+                answer += "최대 승객수를 넘었습니다. \n 다음 택시를 이용해주세요";
+                this.setNowPassenger(this.getNowPassenger()- num);
+                this.setMaxPassenger(30 - this.getNowPassenger());
+            }else {
+                if(targetDistance > 1){
+                    int extraDis = targetDistance - 1;
+                    this.target = target;
+                    this.targetDistance = targetDistance;
+                    this.setState("운행중");
+                    this.totalFee += (this.getFee() + (extraDis * 1000));
+                    answer += "승객이 " + num+"명만큼 늘었습니다. \n 잔여 이용객 수는 " +this.getMaxPassenger() + "입니다. \n 목적지는 " + target +"입니다. \n 요금은 " + (this.getFee() + (extraDis * 1000))+ "원 입니다.";
+
+                }else {
+                    this.target = target;
+                    this.targetDistance = targetDistance;
+                    this.setState("운행중");
+                    this.totalFee += this.getFee();
+                    answer += "기본요금 입니다. \n승객이 " + num+"명만큼 늘었습니다. \n 잔여 이용객 수는 " +this.getMaxPassenger() + "입니다. \n 목적지는 " + target +"입니다. \n 요금은 " + this.getFee() + "원 입니다.";
+                }
+
+            }
+        }else {
+            answer += "운행중인 택시가 아니므로 다른 택시를 이용해 주세요";
+        }
+        return answer;
+    }
+
+    @Override
+    public  void changeAmount(int fuel){
+        this.setAmount(this.getAmount()+(fuel));
+        if (fuel < 0){
+            if (this.getAmount() < 10){
+                this.setState("운행불가");
+                System.out.println("주유량 : " + this.getAmount());
+                System.out.println("상태 : " + this.getState());
+                System.out.println("주유가 필요합니다");
+            } else {
+                System.out.println("주유량 : " + this.getAmount());
+            }
+        }else {
+            System.out.println("주유량 : " + this.getAmount());
+            System.out.println("상태 : " + this.getState());
+        }
+    }
+
+    public void payment(){
+       if(this.getAmount() < 10){
+           this.setState("운행불가");
+           this.setNowPassenger(0);
+           this.setMaxPassenger(4);
+           System.out.println("상태 : " + this.getState());
+           System.out.println("누적요금 : " + this.totalFee);
+           System.out.println("주유가 필요합니다");
+       }else {
+           this.setState("일반");
+           this.setNowPassenger(0);
+           this.setMaxPassenger(4);
+           System.out.println("상태 : " + this.getState());
+           System.out.println("누적요금 : " + this.totalFee);
+       }
+    }
 }
--- src/Day21/TaxiTest.java
+++ src/Day21/TaxiTest.java
@@ -2,6 +2,28 @@ package Day21;
 
 public class TaxiTest {
     public static void main(String[] args) {
+        int number = (int) (Math.random() * (9999 - 1000 + 1)) + 1000;
+       Taxi taxi = new Taxi( number, 100);
+
+        int number1 = (int) (Math.random() * (9999 - 1000 + 1)) + 1000;
+        if(number1 != number){
+            Taxi taxi1 = new Taxi(number1, 60);
+            System.out.println("서로다른 두 차가 생성 되었습니다.");
+        }else {
+            number1 = (int) (Math.random() * (9999 - 1000 + 1)) + 1000;
+            System.out.println("새로운 차 번호를 생성했습니다. : " + number1);
+        }
+
+        System.out.println(taxi.startDriving());
+        System.out.println("주유량 : " + taxi.getAmount());
+        System.out.println(taxi.insertPassenger(2, "서울역", 2));
+        System.out.println("상태 : " + taxi.getState());
+        taxi.changeAmount(-80);
+        taxi.payment();
+        System.out.println(taxi.insertPassenger(5, "삼각지", 5));
+        System.out.println(taxi.insertPassenger(3, "구로디지털단지", 12));
+        taxi.changeAmount(-20);
+        taxi.payment();
 
     }
 }
