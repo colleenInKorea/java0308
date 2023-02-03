aa7774b8b2be7298d81fead7059fe87ca904b035
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-03 15:08:19 +0900

    Day21

---
+++ src/Day21/Bus.java
@@ -0,0 +1,88 @@
+package Day21;
+
+public class Bus extends PublicTransport {
+    Bus (int number, int amount, int speed,  int nowPassenger){
+        this.setNumber(number);
+        this.setAmount(amount);
+        this.setSpeed(speed);
+        this.setMaxPassenger(30);
+        this.setNowPassenger(nowPassenger);
+        this.setState("운행중");
+        this.setFee(1000);
+    }
+
+    @Override
+     public String startDriving(){
+        String answer = this.getState();
+        return  answer + "입니다.";
+    }
+
+    @Override
+    public String changeSp(int changeSpeed) {
+        String answer = "";
+        this.setSpeed(this.getSpeed() + (changeSpeed));
+        if(this.getAmount() < 10){
+            System.out.println( "주유량 : " + this.getAmount());
+            System.out.println("주유량을 확인해주세요");
+            answer += "속도가 " + this.getSpeed() +"으로 변경 되었습니다.";
+        }else {
+            answer += "속도가 " + this.getSpeed() +"으로 변경 되었습니다." ;
+        }
+        return answer;
+    }
+
+    @Override
+    public String insertPassenger(int num) {
+        String answer = "";
+
+        if(this.getState().equals("운행중")){
+            this.setNowPassenger(this.getNowPassenger() + num);
+            this.setMaxPassenger(30 - this.getNowPassenger());
+            if(this.getNowPassenger() > 30){
+                answer += "최대 승객수를 넘었습니다. 다음 버스를 이용해주세요";
+            }else {
+                answer += "승객이 " + num+"명만큼 늘었습니다. 잔여 이용객 수는 " +this.getMaxPassenger() + "입니다.";
+            }
+        }else {
+            answer += "운행중인 버스가 아니므로 다른 버스를 이용해 주세요";
+        }
+        return answer;
+    }
+
+    @Override
+    public void changeState() {
+        if(this.getState().equals("운행중")){
+            if(this.getAmount() < 10){
+                this.setState("차고지행");
+                this.setNowPassenger(0);
+                this.setMaxPassenger(30);
+                this.setFee(0);
+            }else {
+                this.setState("운행중");
+            }
+        }else {
+            this.setState("차고지행");
+            this.setNowPassenger(0);
+            this.setMaxPassenger(30);
+            this.setFee(0);
+        }
+    }
+
+    @Override
+    public  void changeAmount(int fuel){
+        this.setAmount(this.getAmount()+(fuel));
+        if (fuel < 0){
+            if (this.getAmount() < 10){
+                this.setState("차고지행");
+                System.out.println("주유량 : " + this.getAmount());
+                System.out.println("상태 : " + this.getState());
+                System.out.println("주유가 필요합니다");
+            } else {
+                System.out.println("주유량 : " + this.getAmount());
+            }
+        }else {
+            System.out.println("주유량 : " + this.getAmount());
+            System.out.println("상태 : " + this.getState());
+        }
+    }
+}
---
+++ src/Day21/PublicTransport.java
@@ -0,0 +1,103 @@
+package Day21;
+
+public abstract class PublicTransport {
+    private int number;
+    private int amount = 100;// 주유량
+    private int speed = 0;//속도
+    private int maxPassenger;//최대 승객수
+    private String state;//상태
+
+
+
+    private int nowPassenger;//현재승객수
+    private int fee ;//요금
+
+
+    //getter, setter 만들기
+    public int getNowPassenger() {
+        return nowPassenger;
+    }
+
+    public void setNowPassenger(int nowPassenger) {
+        this.nowPassenger = nowPassenger;
+    }
+
+    public int getFee() {
+        return fee;
+    }
+
+    public void setFee(int fee) {
+        this.fee = fee;
+    }
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getMaxPassenger() {
+        return maxPassenger;
+    }
+
+    public void setMaxPassenger(int maxPassenger) {
+        this.maxPassenger = maxPassenger;
+    }
+
+    //공통요소 기능 만들기
+    //운행시작
+    public String startDriving() {
+        String answer = "";
+        return answer;
+    }
+
+    //속도 변경
+    public String changeSp(int changeSpeed) {
+        String answer = "";
+        return answer;
+    }
+
+    //상태 변경
+    public void changeState() {
+
+    }
+
+    //승객 탑승
+    public String insertPassenger(int num) {
+        String answer = "";
+//        int sum =
+        return answer;
+    }
+
+    //주유량 변화
+    public  void changeAmount(int fuel){
+
+    }
+
+
+}
